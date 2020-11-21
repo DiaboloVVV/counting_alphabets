c7019db3b8ac50b4e4af1b30f8b83ba2ffbfe4e5
DiaboloVVV <38590830+DiaboloVVV@users.noreply.github.com>
2020-11-21 10:52:24 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-# def print_hi(name):
-#    # Use a breakpoint in the code line below to debug your script.
-#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-#     print_hi('PyCharm')
-
-
 def opening_file(file_name):  # function to open an file and read data from it
     with open(file_name, 'r') as r:
         data = r.read()  # saving output into variable called data
